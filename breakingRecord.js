87e4e0b35547cab51560a08aaa7cea3a557660bc
andrewbraunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-07-16 00:40:19 +0000

    breaking records

---
+++ breakingRecord.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the breakingRecords function below.
+function breakingRecords(scores) {
+
+    var minimum = scores[0];
+    var maximum = scores[0];
+    var minCount = 0;
+    var maxCount = 0;
+    for (var i = 0; i < scores.length; i++){
+        if (minimum < scores[i]) {
+            minimum = scores[i];
+            minCount++;
+        } else if ( maximum > scores[i]){
+            maximum = scores[i];
+            maxCount++;
+        }
+    }
+
+    return [minCount, maxCount];
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const scores = readLine().split(' ').map(scoresTemp => parseInt(scoresTemp, 10));
+
+    const result = breakingRecords(scores);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
